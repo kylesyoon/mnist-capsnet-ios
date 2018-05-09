f1ec1e0a3226d3416a6b41351333af6aaa0d77ed
Kyle Yoon <kyle.yoon@grainger.com>
2018-05-09 15:52:45 -0500

    Basic drawing interface for hitting the mnist-capsnet-api

--- MNIST-Client/ViewController.swift
+++ MNIST-Client/ViewController.swift
@@ -7,19 +7,79 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    var lastPoint = CGPoint.zero
+    @IBOutlet weak var tempImageView: UIImageView!
+    @IBOutlet weak var predictionLabel: UILabel!
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        self.lastPoint = touch.location(in: self.tempImageView)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        let currentPoint = touch.location(in: self.tempImageView)
+        self.drawLineFrom(fromPoint: self.lastPoint, toPoint: currentPoint)
+        
+        self.lastPoint = currentPoint
     }
-
-
+    
+    func drawLineFrom(fromPoint: CGPoint, toPoint: CGPoint) {
+        // 1
+        UIGraphicsBeginImageContext(self.tempImageView.frame.size)
+        guard let context = UIGraphicsGetCurrentContext() else { return }
+        self.tempImageView.image?.draw(in: self.tempImageView.bounds)
+        
+        // 2
+        context.move(to: fromPoint)
+        context.addLine(to: toPoint)
+        
+        // 3
+        context.setLineCap(.round)
+        context.setLineWidth(10.0)
+        context.setStrokeColor(UIColor.black.cgColor)
+        context.setBlendMode(.normal)
+        
+        // 4
+        context.strokePath()
+        
+        // 5
+        self.tempImageView.image = UIGraphicsGetImageFromCurrentImageContext()
+        self.tempImageView.alpha = 1.0
+        UIGraphicsEndImageContext()
+    }
+    
+    @IBAction func didTapSend(_ sender: Any) {
+        guard let imageData = UIImageJPEGRepresentation(self.tempImageView.image!, 0.2) else { return }
+        
+        Alamofire.upload(multipartFormData: { multipartFormData in
+            multipartFormData.append(imageData, withName: "predict_image", fileName: "number.jpg", mimeType: "image/jpg")
+        },
+                         to: "http://127.0.0.1:8000/capsnet/predict/",
+                         encodingCompletion: { result in
+                            switch result {
+                            case .success(let upload, _, _):
+                                upload.uploadProgress(closure: { progress in
+                                    print("PROGRESS: \(progress.fractionCompleted)")
+                                })
+                                
+                                upload.responseJSON(completionHandler: { response in
+                                    guard
+                                        let json = response.result.value as? [String: Int],
+                                        let prediction = json["prediction"] else { return }
+                                    self.predictionLabel.text = "\(prediction)"
+                                })
+                            case .failure(let error):
+                                print(error)
+                            }
+        })
+    }
+    
+    @IBAction func didTapClear(_ sender: Any) {
+        self.tempImageView.image = nil
+    }
 }
 
---
+++ Podfile
@@ -0,0 +1,5 @@
+use_frameworks!
+
+target 'MNIST-Client' do
+	pod 'Alamofire'
+end
